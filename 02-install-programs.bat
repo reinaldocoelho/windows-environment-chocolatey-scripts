2d474dc7ed9a133b8939ba1bb9073a8b05b481b1
reinaldo <reinaldo.coelho@gmail.com>
2018-06-04 22:26:56 -0300

    Ajuste script instalação

--- 02-install-programs.bat
+++ 02-install-programs.bat
@@ -1,35 +1,38 @@
-﻿# Programas basicos
+﻿@echo off
+REM Programas basicos
 cinst 7zip -y
 cinst GoogleChrome -y
 cinst ChocolateyGUI -y
 cinst notepadplusplus -y
 
-# Office
+REM Office
 cinst libreoffice -y
 
-# Desenvolvimento
-cinst DotNet4.5.2 -y
+REM Desenvolvimento
+REM cinst DotNet4.5.2 -y
 cinst DotNet4.6.1 -y
-cinst dotnetcore -y
+REM cinst dotnetcore -y
 cinst dotnetcore-sdk -y
 cinst git -y
 cinst gitextensions -y
-cinst linqpad -y
+REM cinst linqpad -y
+cinst dbeaver -y
 cinst nodejs -y
 cinst visualstudio2017community -y
 cinst visualstudiocode -y
-cinst mysql.workbench -y
+REM cinst mysql.workbench -y
+cinst mysql-cli -y
 cinst kdiff3 -y
-cinst plantuml -y ## Programa de desenho UML atraves de texto
-# Desenvolvimento opcionais
-# cinst postman -y ## Programa para testes de urls, ótimo para validar e testar interface REST
-# cinst catchlight -y ## Programa para acompanhamento de builds
+cinst plantuml -y REM Programa de desenho UML atraves de texto
+REM Desenvolvimento opcionais
+REM cinst postman -y ## Programa para testes de urls, ótimo para validar e testar interface REST
+REM cinst catchlight -y ## Programa para acompanhamento de builds
 cinst putty -y
 cinst winscp -y
 cinst dotnet.script -y ## Permite a execução de .csx usando c# para scripts multi-plataforma.
 choco install -y cake.portable ## Cake build.
 
-# Meus Programas
+REM Meus Programas
 cinst dropbox -y
 cinst rdcman -y
 cinst skype -y
@@ -38,17 +41,21 @@ cinst vlc -y
 cinst iTunes -y
 cinst slack -y
 
-# Jogos
+REM Jogos
 cinst steam -y
 
-# Container
-## Para o docker funcionar, e necessario estar com o windows atualizado e com a Feature Container instalada.
+REM Container
+REM  Para o docker funcionar, e necessario estar com o windows atualizado e com a Feature Container instalada.
 cinst docker -y
 cinst docker-compose -y
 cinst docker-machine -y
 cinst docker-for-windows -y
 
+REM VMs as a Code
+REM cinst vagrant -y
+cinst terraform -y
+REM cinst ??? -y
 
-# Projetos sem Chocolatey no momento
-## cinst cerebro -y ## https://cerebroapp.com/
-## cinst vsts-cli -y ## https://docs.microsoft.com/pt-br/cli/vsts/install?view=vsts-cli-latest
+REM Projetos sem Chocolatey no momento
+REM  cinst cerebro -y ## https://cerebroapp.com/
+REM  cinst vsts-cli -y ## https://docs.microsoft.com/pt-br/cli/vsts/install?view=vsts-cli-latest
